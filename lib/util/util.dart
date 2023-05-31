01685b21f34248ebc01510821e55c5a7d16bdf8b
Bruno Campera <brunocmpr@gmail.com>
2023-05-31 00:46:01 -0300

    Shows current date and time in the app bar

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,6 +1,7 @@
 import 'package:intl/intl.dart';
 
 String formatDateTime(DateTime dateTime) => DateFormat('dd/MM/yyyy HH:mm').format(dateTime);
+String formatDateTimeSeconds(DateTime dateTime) => DateFormat('dd/MM/yyyy HH:mm:ss').format(dateTime);
 String formatDate(DateTime dateTime) => DateFormat('dd/MM/yyyy').format(dateTime);
 
 String? weekdayStringFromInteger(int dayValue) {
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
 
 import 'package:app3idade_patient/models/dose.dart';
+import 'package:app3idade_patient/services/auth_service.dart';
 import 'package:app3idade_patient/services/dose_service.dart';
+import 'package:app3idade_patient/util/util.dart';
 import 'package:app3idade_patient/widgets/dose_list.dart';
 import 'package:app3idade_patient/widgets/dose_display.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   final String token;
@@ -17,20 +20,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final _doseService = DoseService();
+  final _authService = AuthService();
   List<Dose>? _doses;
   Dose? _selectedDose;
   static const double _padding = 16;
   static const int _refreshIntervalMinutes = 10;
   late Timer _timer = Timer.periodic(const Duration(minutes: _refreshIntervalMinutes), (Timer t) => _loadData());
+  String? _currentDateTime;
 
   @override
   void initState() {
     super.initState();
+    _updateDateTime();
     _loadData();
     if (_doses != null && _doses!.isNotEmpty) {
       _selectedDose = _doses![0];
     }
-    startTimer();
+    _startTimer();
   }
 
   @override
@@ -49,16 +55,43 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void startTimer() {
+  void _startTimer() {
     if (_timer.isActive) return;
     _timer = Timer.periodic(const Duration(minutes: _refreshIntervalMinutes), (Timer t) => _loadData());
+  }
+
+  String _getGreetings() {
+    var hour = DateTime.now().hour;
+    if (hour >= 5 && hour < 12) {
+      return "Bom dia";
+    } else if (hour >= 12 && hour < 18) {
+      return "Boa tarde";
+    }
+    return "Boa noite";
+  }
+
+  void _updateDateTime() {
+    final now = DateTime.now();
+    var formattedDateTime = formatDateTimeSeconds(now);
+    setState(() {
+      _currentDateTime = formattedDateTime;
+    });
+    Future.delayed(const Duration(seconds: 1)).then((_) => _updateDateTime());
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: Center(child: Text('${_getGreetings()}! ${_currentDateTime ?? ''}')),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _authService.logoutAndGoToLogin(context);
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       body: Container(
         decoration: const BoxDecoration(color: Colors.greenAccent),
@@ -78,7 +111,7 @@ class _HomePageState extends State<HomePage> {
                   refreshRequested: (value) async {
                     _timer.cancel();
                     await _loadData();
-                    startTimer();
+                    _startTimer();
                   },
                 ),
               ),
